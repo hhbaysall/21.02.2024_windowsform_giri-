5127765091fb4b28ef742872a6e2980c57f6dda1
Halil İbrahim Baysal <hhbaysall@gmail.com>
2024-02-21 20:56:43 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace _21._02._2024_windowsform_giriş
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
